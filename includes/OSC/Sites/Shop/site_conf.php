632020693f080105f3d1e2bb7156a7a4fd30f5ca
Henry Withoot <osc2nuke@gmail.com>
2017-10-19 15:02:55 +0200

    no message

--- includes/OSC/Sites/Shop/site_conf.php
+++ includes/OSC/Sites/Shop/site_conf.php
@@ -1,9 +1,9 @@
 <?php
 $ini = <<<EOD
-dir_root = ""
-http_server = ""
-http_path = ""
+dir_root = "D:/wamp64/www/GitHub/oscompose/"
+http_server = "http://localhost"
+http_path = "/Github/oscompose/"
 http_images_path = "images/"
 http_cookie_domain = ""
-http_cookie_path = ""
+http_cookie_path = "/Github/oscompose/"
 EOD;
